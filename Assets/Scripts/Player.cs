91a4cf651fa06804d3692693f30f582b17f95e82
Fede <fedemc@gmail.com>
2018-03-31 21:06:05 -0300

    Control for the ship

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In meters per second")][SerializeField] float acceleration=4f;
+    [SerializeField] float xLimit = 5.5f;
+    [SerializeField] float yLimit = 3.5f;
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -30f;
+    [SerializeField] float positionYawFactor = 5f;
+    [SerializeField] float controlRollFactor = -30f;
+    float xThrow, yThrow;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ManageTranslation();
+        ManageRotation();
+    }
+
+    private void ManageRotation()
+    {
+        float pitch= transform.localPosition.y * positionPitchFactor + yThrow*controlPitchFactor;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ManageTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * acceleration * Time.deltaTime;
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float newXPos = Mathf.Clamp(rawNewXPos, -xLimit, xLimit);
+
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * acceleration * Time.deltaTime;
+        float rawNewYPos = transform.localPosition.y + yOffset;
+        float newYPos = Mathf.Clamp(rawNewYPos, -yLimit, yLimit);
+
+        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
+    }
+}
